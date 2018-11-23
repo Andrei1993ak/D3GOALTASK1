ebd36bfb2d7cdf894263f3fb7e3740e1b2d155c2
Andrei Kazlouski <Andrei19.93ak@gmail.com>
2018-11-24 00:39:03 +0300

    Task 1 : Create base version of App : DONE

---
+++ app/src/main/java/com/github/andrei1993ak/mentoring/task1/StringUtil.java
@@ -0,0 +1,5 @@
+package com.github.andrei1993ak.mentoring.task1;
+
+public class StringUtil {
+    public static final String EMPTY = "";
+}
---
+++ app/src/main/java/com/github/andrei1993ak/mentoring/task1/operations/MathOperation.java
@@ -0,0 +1,6 @@
+package com.github.andrei1993ak.mentoring.task1.operations;
+
+public interface MathOperation {
+
+    Double execute(final Double pOperand1, final Double pOerand2);
+}
